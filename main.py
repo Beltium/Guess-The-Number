a4de23e71ab9ce027379058235f0339f4811ff4a
beltium <mathieuhugo2008@gmail.com>
2024-12-19 08:39:47 +0100

    Fonction rand_nd()

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 
+from random import randint
+
+def rand_nb(difficulty):
+    range = [100, 1000, 10000]
+    return randint(1, range[difficulty])
+
+
+
+def game():
+    return 0
+
 # Fonction principale
 def main():
     print("Hello World!")
 
 
 if __name__ == '__main__':
-    main()
