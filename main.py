5be2ac55621885afb2ff3ffced749a00272b4870
beltium <mathieuhugo2008@gmail.com>
2024-12-20 08:13:17 +0100

    Fonction choose_difficulty()

--- main.py
+++ main.py
@@ -11,14 +11,19 @@ def choose_difficulty():
     print("1. Facile (1-100, 30 coups)")
     print("2. Moyen (1-1000, 20 coups)")
     print("3. Difficile (1-10000, 10 coups)")
-    try :
-        choice = int(input("Votre choix : "))
-    except ValueError:
-        print("Veuillez rentrer un nombre.")
-    if choice not in [1, 2, 3]:
-        print("Veuillez rentrez un nombre entre 1 et 3")
+    while True:
+        try:
+            choice = int(input("Votre choix : "))
+        except ValueError:
+            print("Veuillez rentrer un nombre.")
+            continue
+        if choice not in [1, 2, 3]:
+            print("Veuillez rentrez un nombre entre 1 et 3")
+            continue
+        break
     attempts = {1: 30, 2: 20, 3: 10}
-    return choice, attempts[choice]
+    range = {1: 100, 2: 1000, 3: 10000}
+    return choice,range[choice] , attempts[choice]
 
 def game():
     return 0
